3e2eb45ddc75a2504357afd083ebb18aa4dbe5dd
cyobi164 <mrgnchl090@gmail.com>
2025-02-12 01:49:33 +0900

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import ProductMain from "./components/ProductMain";
-import ProductCard from "./components/ProductCard";
+
 import ProductHeader from "./components/ProductHeader";
 
 
